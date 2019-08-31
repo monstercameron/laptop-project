b6634c1d4dd6175aadb1474b23f46e773d94aa85
Earl Cameron <mr.e.cameron@gmail.com>
2019-08-31 18:56:10 -0400

    Updates

---
+++ precache-manifest.8cd25b378159b37b08417bd9ddee34c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30135c9a6eebd5b70c858ae4b8133631",
+    "url": "/laptop-project/index.html"
+  },
+  {
+    "revision": "59fb64d6f61f9c6fafdb",
+    "url": "/laptop-project/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "0df596215ca6ab1c4150",
+    "url": "/laptop-project/static/js/2.1cabd16e.chunk.js"
+  },
+  {
+    "revision": "59fb64d6f61f9c6fafdb",
+    "url": "/laptop-project/static/js/main.5a08164a.chunk.js"
+  },
+  {
+    "revision": "4ce05e5ebd1dfc9987a7",
+    "url": "/laptop-project/static/js/runtime~main.c9001425.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/laptop-project/static/media/logo.5d5d9eef.svg"
+  }
+]);
